8a0244a5814fc85bc68e4a9d4e3622c53266f179
Thanura Rukshan <thanurarukshan2000@gmail.com>
2023-10-01 01:29:12 +0530

    Responsive Navbar

--- src/Headerbar.js
+++ src/Headerbar.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import "./Headerbar.css";
 import { Link } from 'react-router-dom';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import { useRef } from 'react';
 
 function Headerbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive-nav");
+  }
+
   return (
     <div className='headerbarMain'>
-        <div className='headerbarMain-dir'>
-            <Link to="/"><span>Home</span> </Link>
-            <Link to="/library"><span>Library</span> </Link>
-            <Link to="/join"><span>Join_With_the_Community</span> </Link>
-            <Link to="/about"><span>About_Us</span> </Link>
+        <div className='headerbarMain-dir' ref={navRef}>
+            <Link to="/" onClick={showNavbar}><span>Home</span> </Link>
+            <Link to="/library" onClick={showNavbar}><span>Library</span> </Link>
+            <Link to="/join" onClick={showNavbar}><span>Join_With_the_Community</span> </Link>
+            <Link to="/about" onClick={showNavbar}><span>About_Us</span> </Link>
+
         </div>
+        <button className='nav-btn nav-open-btn' onClick={showNavbar} >
+          <FaBars />
+        </button>
     </div>
   )
 }
