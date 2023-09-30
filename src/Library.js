35e8aa8591636e8b52afe917a5b69ef456a07563
Thanura Rukshan <thanurarukshan2000@gmail.com>
2023-09-30 13:07:37 +0530

    Font issue fixed

--- src/Library.js
+++ src/Library.js
@@ -4,7 +4,9 @@ import "./Library.css";
 function Library() {
   return (
     <div className='library-main'>
-      <img src='../Assets/Asset02.svg' className='library-img' alt=''/>
+      <img src='../Assets/Asset03.svg' className='library-img' alt=''/>
+      <span className='library-main-text'>We are Under Maintenance</span>
+      <span className='library-main-text02'>Will be Back Soon!</span>
     </div>
   )
 }
