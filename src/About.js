6d2164d194bc789dde04c5b2d288beee0769f958
Thanura Rukshan <thanurarukshan2000@gmail.com>
2023-09-30 09:59:12 +0530

    About Updated

--- src/About.js
+++ src/About.js
@@ -6,7 +6,12 @@ function About() {
     <div className='aboutusMain'>
       <div className='aboutusMain-content'>
         <span className='aboutusMain-content-title'>Our Team</span>
-        <span className='aboutusMain-content-content'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</span>
+        <span className='aboutusMain-content-content'>
+          <p>Welcome to "Unihelper", a dedicated platform crafted by a passionate team of engineering undergraduates with one common goal - to provide valuable support to our fellow undergraduates and the wider academic community. We understand the challenges that come with pursuing higher education, and we're here to make your journey smoother and more successful.</p>
+          <p>As fellow students, we've walked in your shoes and understand the unique struggles and questions that can arise during your academic journey. That's why we've come together to create this resource-rich website, a one-stop destination for all your university-related needs.</p>
+          <p>Our team's collective experience spans various fields of Engineering, IT, Computer Science, ensuring that the information and resources we provide are not only accurate but also tailored to your specific needs. Whether you're looking for study tips, career advice, or guidance on navigating campus life, we've got you covered.</p>
+          <p>We believe in the power of knowledge sharing and community support. This website is our way of giving back to the academic community that has nurtured us. Together, let's unlock the full potential of your university experience. Explore our comprehensive guides, engage with our blog posts, and connect with us through our community forums. Your success is our success, and we're here to help you every step of the way.</p>
+        </span>
       </div>
     </div>
   )
